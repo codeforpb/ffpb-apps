2e810808270ca0f582a63a39ab9289eba1617fbc
Jan Lippert <lippertsjan@gmail.com>
2014-11-30 20:58:47 +0100

    initial commit

---
+++ app/src/main/java/net/freifunk/paderborn/ffpb_demo/Main.java
@@ -0,0 +1,13 @@
+package net.freifunk.paderborn.ffpb_demo;
+
+import android.support.v7.app.*;
+
+import org.androidannotations.annotations.*;
+
+/**
+ * Created by ljan on 30.11.14.
+ */
+@EActivity(R.layout.test_layout)
+public class Main extends ActionBarActivity{
+
+}
