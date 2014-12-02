5bd4c9094ba692741abdd75a2e778a739f65257a
Jan Lippert <lippertsjan@gmail.com>
2014-12-02 21:28:18 +0100

    Added json format

--- app/src/main/java/net/freifunk/paderborn/ffpb_demo/json/MetaInformation.java
+++ app/src/main/java/net/freifunk/paderborn/ffpb_demo/json/MetaInformation.java
@@ -1,11 +1,13 @@
 package net.freifunk.paderborn.ffpb_demo.json;
 
+import com.fasterxml.jackson.annotation.*;
+
 import java.util.*;
 
 /**
  * Class to represent json-API metainformation
  */
 public class MetaInformation {
-    /** FIXME: use jackson dateformat pattern: 2014-12-01T20:49:26 */
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss", timezone="CET")
     Date timestamp;
 }
