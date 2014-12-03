139d09e02944b8008372f4d054636a9d2832cd0d
Jan Lippert <lippertsjan@gmail.com>
2014-12-03 14:35:15 +0100

    New json api

--- app/src/main/java/net/freifunk/paderborn/ffpb_demo/json/Api.java
+++ app/src/main/java/net/freifunk/paderborn/ffpb_demo/json/Api.java
@@ -6,11 +6,12 @@ import org.springframework.http.converter.json.*;
 /**
  * An api-interface to get some freifunk-data. Other sources (non-json) include:
  *
- * Krombel's dashboard. Date is available via "last modified":
- * <ul><li>http://counts.paderborn.freifunk.net/aktNodecount.txt</li>
- * <li>http://counts.paderborn.freifunk.net/aktClientcount.txt</li>
- * <li>http://counts.paderborn.freifunk.net/maxNodecount.txt</li>
- * <li>http://counts.paderborn.freifunk.net/maxClientcount.txt</li>
+ * Krombel's dashboard
+ * <ul>
+ *     <li>http://counts.paderborn.freifunk.net/aktNodecount.json</li>
+ *     <li>http://counts.paderborn.freifunk.net/aktClientcount.json</li>
+ *     <li>http://counts.paderborn.freifunk.net/maxNodecount.json</li>
+ *     <li>http://counts.paderborn.freifunk.net/maxClientcount.json</li>
  * </ul>
  */
 @Rest(rootUrl = "http://map.paderborn.freifunk.net/nodes.json", converters = MappingJackson2HttpMessageConverter.class)
