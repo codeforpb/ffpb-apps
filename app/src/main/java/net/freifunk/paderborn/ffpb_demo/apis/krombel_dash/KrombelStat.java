2a06e579c5ab6f671b73967775919590303f3830
Jan Lippert <lippertsjan@gmail.com>
2014-12-03 16:03:46 +0100

    added krombel api

---
+++ app/src/main/java/net/freifunk/paderborn/ffpb_demo/apis/krombel_dash/KrombelStat.java
@@ -0,0 +1,31 @@
+package net.freifunk.paderborn.ffpb_demo.apis.krombel_dash;
+
+import java.util.*;
+
+/**
+ * Created by ljan on 03.12.14.
+ */
+public class KrombelStat {
+    int count;
+    Date timestamp;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public KrombelStat() {
+
+    }
+}
