60a8f9f3f28ca8d3ac73ff13929869f378734ae8
Jan Lippert <lippertsjan@gmail.com>
2014-12-07 19:20:07 +0100

    Added automatic synchronisation

--- krombel/src/main/java/net/freifunk/paderborn/krombel/FfpbDemoApplication.java
+++ krombel/src/main/java/net/freifunk/paderborn/krombel/FfpbDemoApplication.java
@@ -2,9 +2,7 @@ package net.freifunk.paderborn.krombel;
 
 import android.accounts.*;
 import android.app.*;
-
-import com.noveogroup.android.log.*;
-import com.noveogroup.android.log.Logger;
+import android.os.*;
 
 import org.androidannotations.annotations.*;
 import org.androidannotations.annotations.res.*;
@@ -16,6 +14,8 @@ import org.slf4j.*;
 @EApplication
 public class FfpbDemoApplication extends Application {
     public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(FfpbDemoApplication.class);
+    private static final long SYNC_INTERVAL = 4 * 60 * 60 * 1000;
+
     @StringRes
     String accountName, accountType, accountAuthority;
 
@@ -23,21 +23,20 @@ public class FfpbDemoApplication extends Application {
     AccountManager mAccountManager;
 
     @AfterInject
-    void createAccount(){
-        Account newAccount = new Account(
+    void createAccount() {
+        Account account = new Account(
                 accountName, accountType);
 
         /*
          * Add the account and account type, no password or user data
          * If successful, return the Account object, otherwise report an error.
          */
-        if (mAccountManager.addAccountExplicitly(newAccount, null, null)){
-            LOGGER.info("Added account {}.", newAccount);
-        }else{
-            LOGGER.info("Could not add account {}.", newAccount);
+        if (mAccountManager.addAccountExplicitly(account, null, null)) {
+            LOGGER.info("Added account {}.", account);
+        } else {
+            LOGGER.info("Could not add account {}.", account);
         }
 
-
-
+        getContentResolver().addPeriodicSync(account,accountAuthority,Bundle.EMPTY,SYNC_INTERVAL);
     }
 }
