f9b6ab7d7bb242dd9289c509bd9f52f300bda92e
Jan Lippert <lippertsjan@gmail.com>
2014-12-14 00:38:59 +0100

    Extracting AccountCreator

--- krombel/src/main/java/net/freifunk/paderborn/krombel/FfpbDemoApplication.java
+++ krombel/src/main/java/net/freifunk/paderborn/krombel/FfpbDemoApplication.java
@@ -5,8 +5,9 @@ import android.app.*;
 import android.content.*;
 import android.os.*;
 
+import net.freifunk.paderborn.krombel.sync.*;
+
 import org.androidannotations.annotations.*;
-import org.androidannotations.annotations.res.*;
 import org.slf4j.*;
 
 /**
@@ -15,34 +16,22 @@ import org.slf4j.*;
 @EApplication
 public class FfpbDemoApplication extends Application {
     public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(FfpbDemoApplication.class);
+
     private static final long SYNC_INTERVAL = 4 * 60 * 60 * 1000;
 
-    @StringRes
-    String accountName, accountType, accountAuthority;
-
-    @SystemService
-    AccountManager mAccountManager;
+    @Bean
+    AccountCreator mAccountCreator;
 
     @AfterInject
     void createAccount() {
-        Account account = new Account(
-                accountName, accountType);
+        Account account = mAccountCreator.createAccount();
 
-        /*
-         * Add the account and account type, no password or user data
-         * If successful, return the Account object, otherwise report an error.
-         */
-        if (mAccountManager.addAccountExplicitly(account, null, null)) {
-            LOGGER.info("Added account {}.", account);
-        } else {
-            LOGGER.info("Could not add account {}.", account);
-        }
-
-        getContentResolver().addPeriodicSync(account, accountAuthority, Bundle.EMPTY, SYNC_INTERVAL);
+        String authority = mAccountCreator.getAuthority();
+        getContentResolver().addPeriodicSync(account, authority, Bundle.EMPTY, SYNC_INTERVAL);
 
         Bundle forceBundle = new Bundle();
         forceBundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
-        getContentResolver().requestSync(account, accountAuthority, forceBundle);
+        getContentResolver().requestSync(account, authority, forceBundle);
         LOGGER.debug("Setup synchronization and forced sync.");
     }
 }
---
+++ krombel/src/main/java/net/freifunk/paderborn/krombel/sync/AccountCreator.java
@@ -0,0 +1,49 @@
+package net.freifunk.paderborn.krombel.sync;
+
+import android.accounts.*;
+
+import org.androidannotations.annotations.*;
+import org.androidannotations.annotations.res.*;
+import org.slf4j.*;
+
+/**
+ * Created by ljan on 13.12.14.
+ */
+@EBean(scope = EBean.Scope.Singleton)
+public class AccountCreator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountCreator.class);
+
+    @StringRes
+    String accountName, accountType, accountAuthority;
+
+    @SystemService
+    AccountManager mAccountManager;
+    private Account mAccount;
+
+    public Account createAccount() {
+        mAccount = new Account(
+                accountName, accountType);
+
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (mAccountManager.addAccountExplicitly(mAccount, null, null)) {
+            LOGGER.info("Added account {}.", mAccount);
+        } else {
+            LOGGER.info("Could not add account {}.", mAccount);
+        }
+        return mAccount;
+    }
+
+    public Account getAccount() {
+        if (mAccount == null) {
+            createAccount();
+        }
+        return mAccount;
+    }
+
+    public String getAuthority() {
+        return accountAuthority;
+    }
+}
