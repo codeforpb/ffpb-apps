b9b844c6cb3491921f24b1868c541b0c2c724de2
Jan Lippert <lippertsjan@gmail.com>
2014-12-14 00:13:25 +0100

    Fixing NPE

--- krombel/src/main/java/net/freifunk/paderborn/krombel/views/KrombelStatViewGroup.java
+++ krombel/src/main/java/net/freifunk/paderborn/krombel/views/KrombelStatViewGroup.java
@@ -15,7 +15,7 @@ import java.text.*;
  * FIXME doc
  */
 @EViewGroup(R.layout.view_krombel_stat)
-public class KrombelStatViewGroup extends RelativeLayout implements CustomDataView<KrombelStat>  {
+public class KrombelStatViewGroup extends RelativeLayout implements CustomDataView<KrombelStat> {
     private final DateFormat sdf = SimpleDateFormat.getDateTimeInstance();
     @ViewById
     TextView textStatName, textStatValue, textStatDate;
@@ -24,7 +24,10 @@ public class KrombelStatViewGroup extends RelativeLayout implements CustomDataVi
         super(context, attrs);
     }
 
-    public void bind(KrombelStat stat){
+    public void bind(KrombelStat stat) {
+        if (stat == null) {
+            return;
+        }
         textStatName.setText(stat.getType().stringResId);
         textStatValue.setText(stat.getCount() + "");
         textStatDate.setText(sdf.format(stat.getTimestamp()));
